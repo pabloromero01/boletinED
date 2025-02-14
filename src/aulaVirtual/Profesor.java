0dae9be5e745edff80a57b97960bd26088067344
Javier Barceló <javibarpe@hotmail.com>
2025-02-14 13:12:33 +0100

    modificado método calificarAlumno de la clase Profesor

--- src/aulaVirtual/Profesor.java
+++ src/aulaVirtual/Profesor.java
@@ -10,12 +10,14 @@ public class Profesor extends Usuario {
     }
 
 
-    public void calificarAlumno(Alumno alumno, Asignatura asignatura, int nota) {
+    public boolean calificarAlumno(Alumno alumno, Asignatura asignatura, int nota) {
         if (getAsignaturas().contains(asignatura)) {
             alumno.asignarNota(asignatura, nota);
             System.out.println("Nota asignada a " + alumno.getNombre() + " en " + asignatura.getNombre() + ": " + nota);
+            return true;
         } else {
             System.out.println("El profesor no imparte esta asignatura.");
+            return false;
         }
     }
 }
